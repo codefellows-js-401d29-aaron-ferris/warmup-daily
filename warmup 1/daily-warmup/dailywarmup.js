ae5284071d8e0d08bc0a22fee9a5606619b47ba8
abferris <abferris@gmail.com>
2019-03-28 11:05:53 -0700

    daily warmup completed. secondary warmup incoming

--- warmup 1/daily-warmup/dailywarmup.js
+++ warmup 1/daily-warmup/dailywarmup.js
@@ -1,88 +1,127 @@
 'use strict'
-//test array
-const test = [1 ,2 ,3 ,4 ,5 ,6 ,7 ,8 ,9 ,10]
+// test array
+const test = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
 
-///for
-const FORLOOP = function(arr){
-   for (let i = 0; i < arr.length; i++){
-     console.log(arr[i])
+// for loop 
+const FORLOOP = function (arr) {
+  for (let i = 0; i < arr.length; i++) {
+    console.log(arr[i])
   }
-}
+};
+
 FORLOOP(test);
 
-//while
-const WHILELOOP = function(arr){
- let i = 0 ;
-  while(i < arr.length){
-    console.log(arr[i]);
-    i++ ;
+
+// whileloop 
+const WHILELOOP = function (arr) {
+  let i = 0;
+  while (i < arr.length) {
+    console.log(arr[i])
+    i++;
   }
 }
+
 WHILELOOP(test);
 
-// map
-const MAP = function(arr){
-  let result = [];
-  for (let i = 0; i < arr.length; i++){
-    result[i] = arr[i];
-  }
-  console.log(result);
+
+// map function
+const MAP = function (arr) {
+  let output = []
+  for (let i = 0; i < arr.length; i++) {
+    output[i] = arr[i];
+  };
+  console.log(output);
 }
 MAP(test);
 
-//filter
-const FILTER = function(arr,cb) {
-  let result =[];
-  arr.forEach( val => {
+
+//filter function
+const FILTER = function(arr, cb){
+  let result = [];
+  arr.forEach( (val) => {
     if (cb(val)){
-      result.push(val)
+      result.push(val);
     }
-  });
+  })
   console.log(result);
 }
-const fun = function(val){
-  return val%2;
-}
-FILTER(test, fun());
+let funct = (val => (val<5));
 
-//
-const REDUCE = function(arr, cb, init){
-  let acc = (init===undefined) ? undefined : init;
-  for (let i = 0; i< arr.length; i++){
-    acc = callback.call(arr[i],acc);
+FILTER(test, funct)
+
+const REDUCE = function (arr, cb, init){
+  let acc = (init === undefined) ? arr[0]: init;
+  for(let i = 0; i < arr.length; i++){
+    acc = cb(acc, arr[i])
   }
+  console.log(acc)
 }
-console.log(REDUCE);
+
+let fun =(valOne, valTwo)  => {
+  return valOne+valTwo;
+};
+
+REDUCE(test, fun, 0);
 
 //constants given for written functions
-const people = ['Kookla','Fran','Ollie'];
+const people = ['Kookla', 'Fran', 'Ollie'];
 
 const stuff = {
   tv: 'huge',
   radio: 'old',
   toothbrush: 'frayed',
-  cars: ['Toyota','Mazda']
+  cars: ['Toyota', 'Mazda']
 }
 
 let state = {
   people: people,
-  stuff: stuff
-};
-// add with spread to array
-let newPeople = ['Odie', ...people , 'Garfield' ];
-//add to cars in newstuff with spread
-let newStuff = {...stuff ,
-  cars:[...stuff.cars,'1998 Honda Civic LX']
+  stuff: stuff, 
+  testarray: test
 };
 
-//add to people and cars in state with spread
-let newState = {...state,
-  people:['Odie', ...state.people, 'Garfiled'],
-  stuff: {...state.stuff,
-    cars: [...state.stuff.cars, '1998 Honda Civic LX']
-  }
-};
-console.log('people', people, 'newPeople', newPeople);
-console.log('stuff', stuff, 'newStuff', newStuff);
-console.log('state', state , 'newState', newState);
+
+// add odie and garfield to people with spread operator
+let newPeople = ['Odie', ... people, 'Garfield'];
+console.log(newPeople);
+
+
+//add another car to cars in newstuff with spread 
+
+let newStuff = {
+  ...stuff,
+  cars: [...stuff.cars, 'Toyota Supra']
+}
+console.log(newStuff)
+
+//add to people in state  and cars in state with spread
+
+let newState = {
+  ... state,
+  stuff: {
+    ...state.stuff,
+    cars: [...state.stuff.cars, 'Ferarri']
+  } ,
+  people: ['Odie', ...state.people, 'Garfield', 'Jon'],
+  
+}
+
+console.log(newState);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
